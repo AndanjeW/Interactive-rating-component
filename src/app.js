cbb5385a61507f6968c0530cef19f114f3d21999
AndanjeW <94687120+AndanjeW@users.noreply.github.com>
2022-08-26 17:18:02 +0300

    Added functionality to the submit button. Can hide and display the Rating Segment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
-let person = prompt("Please enter your name", "Harry Potter");
+let content = document.querySelector(".content")
 
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
-}
+let ratingContainer = document.querySelector(".content-rating")
+
+let submitButton = document.getElementById("submit")
+
+let rateButton = document.getElementById("rate-again")
+let rating = document.getElementById("rating")
+let rates = document.querySelectorAll(".btn-rating")
+
+submitButton.addEventListener("click", ()=>{
+  ratingContainer.classList.remove("hidden")
+  content.style.display="none"
+})
+rateButton.addEventListener("click", ()=>{
+  ratingContainer.classList.add("hidden")
+  content.style.display="block"
+})
+
+rates.forEach((rate)=>{
+  rate.addEventListener("click", ()=>{
+rating.innerHTML =rate.innerHTML  })
+})
